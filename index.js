0820397bd975f6373b25ec6c7cf39a39399cd64d
Pål Hamel <pal.hamel@gmail.com>
2020-12-09 15:28:36 +0100

    add function nr 2 and commands section

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 console.log("//..Checking setup..");
-
 // dotenv setup:
 require("dotenv").config();
 // console.log(require('dotenv').config())
 const token = process.env.TOKEN;
 // console.log("the token i get:", token);
 // -----------------------------------
-
 // require discord.js:
 const Discord = require("discord.js");
 // create client:
@@ -18,26 +16,40 @@ console.log("//..Connecting Bot, please wait..");
 client.once("ready", () => {
   console.log("//..Now I'm ready! - Bot running");
 });
+// Discord Bot token access via dotenv const:
+client.login(token);
+// -----------------------------------
 
 // functions:
 function randomNumber(message) {
+  const number = Math.random();
+  message.channel.send(number.toString());
+}
+
+function randomNumber2(message) {
   const number = Math.floor(Math.random());
   message.channel.send(number.toString());
 }
 
-// commands:
 
 
+// commands - match incoming text with functions:
+let commands = new Map();
+commands.set("random", randomNumber);
+commands.set("random2", randomNumber2);
 
+
+// process messages:
 client.on("message", (message) => {
-  if (message.content === "?getcode") {
-    const number = Math.floor(Math.random());
-    message.channel.send(number.toString());
+  if (message.content[0] === "?") {
+    const command = message.content.split(" ")[0].substr(1);
+    if (commands.has(command)) {
+      // Check map if command exists
+      commands.get(command)(message); // Runs command
+    }
   }
 });
 
-// Discord Bot token access via dotenv const:
-client.login(token);
 
 /*
 
