872f03f8ee1a21b7e04eadf8110a789bca674d66
Pål Hamel <pal.hamel@gmail.com>
2020-12-09 13:02:02 +0100

    Add README and clg messages

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+console.log('Checking setup..');
+
 // dotenv setup:
-require('dotenv').config()
-console.log(require('dotenv').config())
-// -----------------------------------
+require("dotenv").config();
+// console.log(require('dotenv').config())
 const token = process.env.TOKEN;
-console.log('the token i get:', token);
+// console.log("the token i get:", token);
 // -----------------------------------
 
 // require the discord.js module
@@ -12,16 +13,18 @@ const Discord = require("discord.js");
 // create the Discord client:
 const client = new Discord.Client();
 
+console.log('Connecting Bot, please wait..');
+
 // when app is ready - write out:
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Now I'm ready!");
 });
 
-client.on('message', message => {
-	if (message.content === '!Hello') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('hello to you too.');
-	}
+client.on("message", (message) => {
+  if (message.content === "!Are you there?") {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send("Yes and I am your father");
+  }
 });
 // Replace the value between the quotes with your token
 client.login(token);
