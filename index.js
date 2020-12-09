47003b0253fbd6ff17c7b740c5fc63a315bc7984
Pål Hamel <pal.hamel@gmail.com>
2020-12-09 11:11:44 +0100

    setup index

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const Discord = require("discord.js");
 
 // create the Discord client:
 const client = new Discord.Client();
+
+// when app is ready - write out:
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!Hello') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('hello to you too.');
+	}
+});
+
+// Replace the value between the quotes with your token
+client.login('your-token-goes-here');
